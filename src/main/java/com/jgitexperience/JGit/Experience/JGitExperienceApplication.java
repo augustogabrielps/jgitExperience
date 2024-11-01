393fb62faebd26b57eeabba17fa6b2998675fba5
Augusto <deuvoid@gmail.com>
2024-11-01 14:57:09 -0300

    initial commit

---
+++ src/main/java/com/jgitexperience/JGit/Experience/JGitExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.jgitexperience.JGit.Experience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGitExperienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGitExperienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgitexperience/JGit/Experience/JGitExperienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgitexperience.JGit.Experience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGitExperienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
