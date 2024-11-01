449705f05c4cfc98d11708064f438f7127fd5168
Augusto <deuvoid@gmail.com>
2024-11-01 15:21:42 -0300

    initializing hello world

---
+++ src/main/java/com/jgitexperience/JGit/Experience/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.jgitexperience.JGit.Experience.controller;
+
+import com.jgitexperience.JGit.Experience.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return helloService.getHelloMessage();
+    }
+}
